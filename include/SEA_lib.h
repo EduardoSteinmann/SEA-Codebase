b2f92552cc45c50c0b10dcdc5bae22ad1a800147
Eduardo <edward.steinmann@outlook.com>
2023-11-17 17:17:14 -0500

    Added in new macros for error handling (compatible with C23 auto type deduction), defering resource deallocation, and updated SEA_error macro to work with C23 compound literals.

--- include/SEA_lib.h
+++ include/SEA_lib.h
@@ -1,5 +1,5 @@
-#ifndef SEA_STR
-#define SEA_STR
+#ifndef SEA_LIB
+#define SEA_LIB
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
@@ -23,7 +23,7 @@ typedef struct SEA_LinearAllocator
 typedef struct SEA_Error
 {
     int error_code;
-    unsigned char* error_message;
+    const unsigned char* error_message;
 } SEA_Error;
 
 //////////////////////////////////////////////////////////////////////
@@ -40,6 +40,7 @@ typedef void* SEA_VoidPtr;
 
 //SEA_Result type declarations
 SEA_Result_declr(SEA_VoidPtr);
+SEA_Result_declr(SEA_LinearAllocator);
 
 //////////////////////////////////////////////////////////////////////
 //*              SEA_Error functions and macros                    *//
@@ -50,11 +51,15 @@ SEA_Result_declr(SEA_VoidPtr);
     static unsigned char string_literal_reference_name[sizeof(string_literal)] = string_literal; \
     SEA_Error error_object_name = { err_code, (unsigned char*)string_literal_reference_name }
 
+#define SEA_Error_new(err_code, string_literal) (SEA_Error) { err_code, (static const char[sizeof(string_literal)]){ string_literal } }
+
 void SEA_Error_print(SEA_Error err);
 
 //Checks if there was an error or not (if the error code is not 0) and returns if there the result_object if there was an error
 //Note: the result object should NOT be an rvalue
 #define SEA_try_error(result_object) if (result_object.err.error_code != 0) return result_object;
+#define SEA_try(result_type_if_failed, result_object) result_object.value; if (result_object.err.error_code != 0) return (SEA_Result(result_type_if_failed)) { (result_type_if_failed){ 0 }, result_object.err }
+#define SEA_try_func(value_object, result_type_if_failed, result_function) auto __SEA_macro_var(_r_) = result_function; if (__SEA_macro_var(_r_).err.error_code != 0) return (SEA_Result(result_type_if_failed)) { (result_type_if_failed){ 0 }, __SEA_macro_var(_r_).err };  value_object = __SEA_macro_var(_r_).value
 
 //////////////////////////////////////////////////////////////////////
 //*             Wrappers for standard allocation functions        *//
@@ -65,10 +70,18 @@ SEA_Result(SEA_VoidPtr) SEA_calloc(size_t num_of_objects, size_t size_of_one_obj
 
 //////////////////////////////////////////////////////////////////////
 //*         SEA_LinearAllocator functions and macros               *//
-SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_new(size_t size_of_memory_block);
-void SEA_LinearAllocator_free (SEA_LinearAllocator* linear_allocator);
-SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc(SEA_LinearAllocator* allocator, size_t size_of_memory_block);
-SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc_resize_allocator_if_needed(SEA_LinearAllocator* allocator, size_t size_of_memory_block);
+SEA_Result(SEA_LinearAllocator) SEA_LinearAllocator_new(size_t size_of_memory_block);
+void SEA_LinearAllocator_free (SEA_LinearAllocator* const linear_allocator);
+SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc(SEA_LinearAllocator* const allocator, size_t size_of_memory_block);
+SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc_resize_allocator_if_needed(SEA_LinearAllocator* const allocator, size_t size_of_memory_block);
+void SEA_LinearAllocator_reset_allocation_position(SEA_LinearAllocator* const allocator);
+SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_realloc(SEA_LinearAllocator* const allocator, SEA_VoidPtr* ptr, size_t current_size, size_t desired_size);
+SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_realloc_resize_allocator_if_needed(SEA_LinearAllocator* const allocator, SEA_VoidPtr* ptr, size_t current_size, size_t desired_size);
+
+//////////////////////////////////////////////////////////////////////
+//*         Quality of life/syntactic macros                      *//
+
+#define SEA_defer(open, close) int __SEA_macro_var(_i_) = 0; for (open; __SEA_macro_var(_i_) < 1; (__SEA_macro_var(_i_)++), close)
 
 //////////////////////////////////////////////////////////////////////
 //*         SEA_lib implementation starts here                    *//
@@ -86,8 +99,7 @@ SEA_Result(SEA_VoidPtr) SEA_malloc(size_t size_of_memory_block)
     SEA_Result(SEA_VoidPtr) result = { .value = ptr, .err = { .error_code = 0, .error_message = NULL } };
     if (!ptr)
     {
-        SEA_Error_new_from_static_string_literal(malloc_failed_err, -1, "Malloc returned null: allocation failed.", malloc_failed_message);
-        result.err = malloc_failed_err;
+        result.err = SEA_Error_new(-1, "Malloc returned null: allocation failed.");
     }
     return result;
 }
@@ -98,8 +110,7 @@ SEA_Result(SEA_VoidPtr) SEA_realloc(SEA_VoidPtr ptr, size_t size_of_memory_block
     SEA_Result(SEA_VoidPtr) result = { .value = ptr, .err = { .error_code = 0, .error_message = NULL } };
     if (!ptr)
     {
-        SEA_Error_new_from_static_string_literal(realloc_failed_err, -1, "Realloc returned null: re-allocation failed, old memory block freed.", realloc_failed_message);
-        result.err = realloc_failed_err;
+        result.err = SEA_Error_new(-1, "Realloc returned null: re-allocation failed");
     }
     return result;
 }
@@ -110,48 +121,37 @@ SEA_Result(SEA_VoidPtr) SEA_calloc(size_t num_of_objects, size_t size_of_one_obj
     SEA_Result(SEA_VoidPtr) result = { .value = ptr, .err = { .error_code = 0, .error_message = NULL } };
     if (!ptr)
     {
-        SEA_Error_new_from_static_string_literal(calloc_failed_error, -1, "Calloc returned null: allocation failed.", calloc_failed_message);
-        result.err = calloc_failed_error;
+        result.err = SEA_Error_new(-1, "Calloc returned null: allocation failed.");
     }
     return result;
 }
 
-SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_new(size_t size_of_memory_block)
+SEA_Result(SEA_LinearAllocator) SEA_LinearAllocator_new(size_t size_of_memory_block)
 {
-    SEA_LinearAllocator* linear_allocator = (SEA_LinearAllocator*) malloc(sizeof(SEA_LinearAllocator));
-    SEA_Result(SEA_VoidPtr) result = { .value = linear_allocator, .err = { .error_code = 0, .error_message = NULL } };
+    SEA_LinearAllocator linear_allocator = { .current_allocation_position = -1, .memory = NULL, .capacity = -1 };
 
-    if (!linear_allocator)
+    SEA_Result(SEA_LinearAllocator) result = { .value = linear_allocator, .err = { .error_code = 0, .error_message = NULL } };
+
+    linear_allocator.memory = (unsigned char*) malloc(size_of_memory_block);
+
+    if (!linear_allocator.memory)
     {
-        SEA_Error_new_from_static_string_literal(linear_allocator_err, -1, "Malloc returned null for the pointer to the SEA_LinearAllocator.", allocator_error_message);
-        result.err = linear_allocator_err;
+        result.err = SEA_Error_new(-1, "Malloc returned null for the memory block: allocation failed.");
         return result;
     }
 
-    linear_allocator->memory = (unsigned char*) malloc(size_of_memory_block);
-
-    if (!linear_allocator->memory)
-    {
-        SEA_Error_new_from_static_string_literal(memory_block_err, -1, "Malloc returned null for the block of memory for the allocator.", malloc_err_message);
-        result.err = memory_block_err;
-        result.value = NULL;
-        free(linear_allocator);
-        return result;
-    }
-
-    linear_allocator->current_allocation_position = 0;
-    linear_allocator->capacity = size_of_memory_block;
+    linear_allocator.current_allocation_position = 0;
+    linear_allocator.capacity = size_of_memory_block;
 
     return result;
 }
 
-void SEA_LinearAllocator_free (SEA_LinearAllocator* linear_allocator)
+void SEA_LinearAllocator_free (SEA_LinearAllocator* const linear_allocator)
 {
     free(linear_allocator->memory);
-    free(linear_allocator);
 }
 
-SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc(SEA_LinearAllocator* allocator, size_t size_of_memory_block)
+SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc(SEA_LinearAllocator* const allocator, size_t size_of_memory_block)
 {
     SEA_Result(SEA_VoidPtr) result = { .value = NULL, .err = { .error_code = 0, .error_message = NULL } };
 
@@ -161,8 +161,7 @@ SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc(SEA_LinearAllocator* allocator
 
     if (new_allocation_position > allocator->capacity)
     {
-        SEA_Error_new_from_static_string_literal(capacity_exceeded_err, -1, "The allocation requested exceeds the remaining room in the allocator.", capacity_exceeded_message);
-        result.err = capacity_exceeded_err;
+        result.err = SEA_Error_new(-1, "The allocation requested exceeds the remaining room in the allocator.");
     }
 
     allocator->current_allocation_position = new_allocation_position;
@@ -172,7 +171,7 @@ SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc(SEA_LinearAllocator* allocator
     return result;
 }
 
-SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc_resize_allocator_if_needed(SEA_LinearAllocator* allocator, size_t size_of_memory_block)
+SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc_resize_if_needed(SEA_LinearAllocator* const allocator, size_t size_of_memory_block)
 {
     SEA_Result(SEA_VoidPtr) result = { .value = NULL, .err = { .error_code = 0, .error_message = NULL } };
 
@@ -185,8 +184,7 @@ SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc_resize_allocator_if_needed(SEA
         result = SEA_realloc(allocator->memory, new_allocation_position * 2);
         if (result.err.error_code != 0)
         {
-            SEA_Error_new_from_static_string_literal(realloc_failed_err, -1, "The linear allocator could not be resized: realloc returned null.", realloc_failed_message);
-            result.err = realloc_failed_err;
+            result.err = SEA_Error_new(-1, "The linear allocator could not be resized: realloc returned null.");
             return result;
         }
         allocator->capacity = new_allocation_position * 2;
@@ -198,6 +196,17 @@ SEA_Result(SEA_VoidPtr) SEA_LinearAllocator_alloc_resize_allocator_if_needed(SEA
 
     return result;
 }
+
+void SEA_LinearAllocator_reset(SEA_LinearAllocator* const allocator)
+{
+    allocator->current_allocation_position = 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+/*                 SEA Internal Macros and Functions               */
+#define __SEA_token_concat_direct(t1, t2) t1 ## t2
+#define __SEA_token_concat(t1, t2) __SEA_token_concat_direct(t1, t2)
+#define __SEA_macro_var(name) __SEA_token_concat(name, __LINE__)
 
 #endif
 
